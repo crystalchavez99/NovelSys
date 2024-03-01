535b6421ea182f329277e2baf33966131410ca0c
crystalchavez99 <chavezcrystal1999@yahoo.com>
2024-02-29 16:51:21 -0800

    create datetime provide for app and infra

---
+++ NovelSys.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NovelSys.Application.Common.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovelSys.Infrastructure.Services
+{
+    public  class DateTimeProvider: IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+
+    }
+}
