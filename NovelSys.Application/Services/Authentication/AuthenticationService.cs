1aa4b086ac8946a1c6342cd9377c1fd6fd205661
Crystal <crystal120599@gmail.com>
2024-03-03 20:10:53 -0800

    apply repository pattern for register to check if user exists or create new user

--- NovelSys.Application/Services/Authentication/AuthenticationService.cs
+++ NovelSys.Application/Services/Authentication/AuthenticationService.cs
@@ -1,4 +1,6 @@
 ﻿using NovelSys.Application.Common.Interfaces.Authentication;
+using NovelSys.Application.Common.Interfaces.Persistence;
+using NovelSys.Domain.Entities;
 
 
 namespace NovelSys.Application.Services.Authentication
@@ -6,10 +8,12 @@ namespace NovelSys.Application.Services.Authentication
     public class AuthenticationService : IAuthenticationService
     {
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
+        private readonly IUserRepository _userRepository;
 
-        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
         {
             _jwtTokenGenerator = jwtTokenGenerator;
+            _userRepository = userRepository;
         }
 
         public AuthenticationResult Register(string firstName,
@@ -18,14 +22,21 @@ namespace NovelSys.Application.Services.Authentication
         string password)
         {
             // if user exists
+            if(_userRepository.GetUserByEmail(email) != null)
+            {
+                throw new Exception("User with given email already exists.");
+            }
 
             // create user (uniq id)
+            var user = new User { FirstName = firstName, LastName = lastName, Email = email, Password = password };
+
+            _userRepository.Add(user);
 
             // create jwt token 
-            Guid userId = Guid.NewGuid();
-            var token = _jwtTokenGenerator.GenerateToken(userId,firstName, lastName);
+           // Guid userId = Guid.NewGuid();
+            var token = _jwtTokenGenerator.GenerateToken(user.Id,firstName, lastName);
 
-            return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, token);   
+            return new AuthenticationResult(user.Id, firstName, lastName, email, token);   
         }
 
         public AuthenticationResult Login(
