351307620c91cac5474d9002ccabd4b825acd4da
Crystal <crystal120599@gmail.com>
2024-03-03 19:59:45 -0800

    create domain entity for user, repo pattern for user to implement into project

---
+++ NovelSys.Application/Common/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using NovelSys.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovelSys.Application.Common.Interfaces.Persistence
+{
+    public interface IUserRepository
+    {
+        User? GetUserByEmail(string email);
+        void Add(User user);
+    }
+}
