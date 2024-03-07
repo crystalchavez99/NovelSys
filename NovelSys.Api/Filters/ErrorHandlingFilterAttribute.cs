05d4dc0213329811330651947fe2adbec644763c
Crystal <crystal120599@gmail.com>
2024-03-07 08:47:57 -0800

    create custome error handling middleware

---
+++ NovelSys.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NovelSys.Api.Filters
+{
+    public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            /*if (context.Exception == null)
+             {
+                 return;
+             }
+             context.Result = new ObjectResult(new
+             { error = context.Exception.Message });*/
+            var exception = context.Exception;
+            context.Result = new ObjectResult(exception);
+        }
+    }
+}
